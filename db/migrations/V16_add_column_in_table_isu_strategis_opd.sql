a501b7d8559889599724ffaedce5b18afedd41e7
zulfikar1609 <83656229+zulfikar1609@users.noreply.github.com>
2026-08-04 20:41:38 +0700

    migrasi kolum di table isu strtegis opd

---
+++ db/migrations/V16_add_column_in_table_isu_strategis_opd.sql
@@ -0,0 +1,6 @@
+ALTER TABLE tb_isu_strategis_opd
+ADD COLUMN id_ppd INT NULL,
+ADD COLUMN id_isu_klhs INT NULL,
+ADD COLUMN id_isu_global INT NULL,
+ADD COLUMN id_isu_nasional INT NULL,
+ADD COLUMN id_isu_regional INT NULL;
