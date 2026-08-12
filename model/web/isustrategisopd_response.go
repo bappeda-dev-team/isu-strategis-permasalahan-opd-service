50f9a6c2c895a12eee8c99be222369b32f3b4320
RyanB1303 <ryan.brilliant.nirwana@gmail.com>
2026-08-13 05:59:43 +0700

    tambah id isu in response

--- model/web/isustrategisopd_response.go
+++ model/web/isustrategisopd_response.go
@@ -4,21 +4,21 @@ import "time"
 
 // @Description Response Isu Strategis
 type IsuStrategisResponse struct {
-	Id               	   int                    `json:"id"`
-	KodeOpd          	   string                 `json:"kode_opd"`
-	NamaOpd          	   string                 `json:"nama_opd"`
-	KodeBidangUrusan 	   string                 `json:"kode_bidang_urusan"`
-	NamaBidangUrusan 	   string                 `json:"nama_bidang_urusan"`
-	TahunAwal        	   string                 `json:"tahun_awal"`
-	TahunAkhir       	   string                 `json:"tahun_akhir"`
+	Id                     int                    `json:"id"`
+	KodeOpd                string                 `json:"kode_opd"`
+	NamaOpd                string                 `json:"nama_opd"`
+	KodeBidangUrusan       string                 `json:"kode_bidang_urusan"`
+	NamaBidangUrusan       string                 `json:"nama_bidang_urusan"`
+	TahunAwal              string                 `json:"tahun_awal"`
+	TahunAkhir             string                 `json:"tahun_akhir"`
 	PotensiPerangkatDaerah string                 `json:"potensi_perangkat_daerah"`
-	IsuKlhs       		   string                 `json:"isu_klhs"`
-	IsuGlobal       	   string                 `json:"isu_global"`
-	IsuNasional       	   string                 `json:"isu_nasional"`
-	IsuRegional       	   string                 `json:"isu_regional"`
-	IsuStrategis     	   string                 `json:"isu_strategis"`
-	CreatedAt        	   time.Time              `json:"created_at"`
-	PermasalahanOpd  	   []PermasalahanResponse `json:"permasalahan_opd"`
+	IsuKlhs                string                 `json:"isu_klhs"`
+	IsuGlobal              string                 `json:"isu_global"`
+	IsuNasional            string                 `json:"isu_nasional"`
+	IsuRegional            string                 `json:"isu_regional"`
+	IsuStrategis           string                 `json:"isu_strategis"`
+	CreatedAt              time.Time              `json:"created_at"`
+	PermasalahanOpd        []PermasalahanResponse `json:"permasalahan_opd"`
 }
 
 type PermasalahanResponse struct {
@@ -70,19 +70,24 @@ type PermasalahanKebelakangResponse struct {
 }
 
 type IsuStrategisKebelakangResponse struct {
-	Id              	   int                              `json:"id"`
-	KodeOpd         	   string                           `json:"kode_opd"`
-	NamaOpd         	   string                           `json:"nama_opd"`
-	KodeBidangUrusan	   string                           `json:"kode_bidang_urusan"`
-	NamaBidangUrusan 	   string                           `json:"nama_bidang_urusan"`
-	TahunAwal        	   string                           `json:"tahun_awal"`
-	TahunAkhir       	   string                           `json:"tahun_akhir"`
-	PotensiPerangkatDaerah string                 	    	`json:"potensi_perangkat_daerah"`
-	IsuKlhs       		   string                 		    `json:"isu_klhs"`
-	IsuGlobal       	   string                 		    `json:"isu_global"`
-	IsuNasional       	   string                 	        `json:"isu_nasional"`
-	IsuRegional       	   string                 		    `json:"isu_regional"`
-	IsuStrategis     	   string                           `json:"isu_strategis"`
-	CreatedAt        	   time.Time                        `json:"created_at"`
-	PermasalahanOpd  	   []PermasalahanKebelakangResponse `json:"permasalahan_opd"`
+	Id                     int                              `json:"id"`
+	KodeOpd                string                           `json:"kode_opd"`
+	NamaOpd                string                           `json:"nama_opd"`
+	KodeBidangUrusan       string                           `json:"kode_bidang_urusan"`
+	NamaBidangUrusan       string                           `json:"nama_bidang_urusan"`
+	TahunAwal              string                           `json:"tahun_awal"`
+	TahunAkhir             string                           `json:"tahun_akhir"`
+	IdPpd                  int                              `json:"id_ppd"`
+	IdIsuKlhs              int                              `json:"id_isu_klhs"`
+	IdIsuGlobal            int                              `json:"id_isu_global"`
+	IdIsuNasional          int                              `json:"id_isu_nasional"`
+	IdIsuRegional          int                              `json:"id_isu_regional"`
+	PotensiPerangkatDaerah string                           `json:"potensi_perangkat_daerah"`
+	IsuKlhs                string                           `json:"isu_klhs"`
+	IsuGlobal              string                           `json:"isu_global"`
+	IsuNasional            string                           `json:"isu_nasional"`
+	IsuRegional            string                           `json:"isu_regional"`
+	IsuStrategis           string                           `json:"isu_strategis"`
+	CreatedAt              time.Time                        `json:"created_at"`
+	PermasalahanOpd        []PermasalahanKebelakangResponse `json:"permasalahan_opd"`
 }
